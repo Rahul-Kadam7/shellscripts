e728afa216969e8bf1e78164f1cc291b4e974b2b
Rahul-Kadam7 <127729018+Rahul-Kadam7@users.noreply.github.com>
2023-05-01 22:58:02 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 #This is my 1st shell script to print output
 echo "Hello All, Welcome to AWS/Devops Class"
-echo "Hello, How are you?"
-echo "Welcome to Young Minds"
-echo "Best class of AWS/DevOps"
-echo "We are learning git"
-echo "We are done with git"
-echo "This example is of branching strategy"
-echo "Hello Batch-16, We are learning
+
 
